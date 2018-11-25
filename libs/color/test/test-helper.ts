9faaf50874d86637af27148c28652d63ba30847b
LinBo Len <linbolen@gradii.com>
2018-11-25 19:45:53 +0800

    rf color add test

--- libs/color/test/test-helper.ts
+++ libs/color/test/test-helper.ts
@@ -1,3 +1,6 @@
+import { Hsl } from '../src/hsl';
+import { Rgb } from '../src/rgb';
+
 export function expectEqualRgb(rgb1, rgb2) {
 
   expect(rgb1.hex()).toBe(rgb2.hex());
@@ -8,4 +11,36 @@ export function expectEqualRgb(rgb1, rgb2) {
 
 export function expectEqualRgba(rgba1, rgba2) {
   expect(rgba1.toString()).toBe(rgba2.toString());
+}
+
+export function rgbEqual(actual, r, g, b, opacity) {
+
+  expect(actual instanceof Rgb
+    && (isNaN(r) ? isNaN(actual.r) && actual.r !== actual.r : Math.round(actual.r) === Math.round(r))
+    && (isNaN(g) ? isNaN(actual.g) && actual.g !== actual.g : Math.round(actual.g) === Math.round(g))
+    && (isNaN(b) ? isNaN(actual.b) && actual.b !== actual.b : Math.round(actual.b) === Math.round(b))
+    && (isNaN(opacity) ? isNaN(actual.opacity) && actual.opacity !== actual.opacity : actual.opacity === opacity)).toBe(true);
+
+  expect([Math.round(actual.r), Math.round(actual.g), Math.round(actual.b), actual.opacity])
+    .toEqual([Math.round(r), Math.round(g), Math.round(b), opacity])
+}
+
+export function rgbStrictEqual(actual, r, g, b, opacity) {
+  expect(actual instanceof Rgb
+    && (isNaN(r) ? isNaN(actual.r) && actual.r !== actual.r : actual.r === r)
+    && (isNaN(g) ? isNaN(actual.g) && actual.g !== actual.g : actual.g === g)
+    && (isNaN(b) ? isNaN(actual.b) && actual.b !== actual.b : actual.b === b)
+    && (isNaN(opacity) ? isNaN(actual.opacity) && actual.opacity !== actual.opacity : actual.opacity === opacity)).toBe(true);
+
+  expect([actual.r, actual.g, actual.b, actual.opacity]).toEqual([r, g, b, opacity]);
+}
+
+export function hslEqual(actual, h, s, l, opacity) {
+  expect(actual instanceof Hsl
+    && (isNaN(h) ? isNaN(actual.h) && actual.h !== actual.h : h - 1e-6 <= actual.h && actual.h <= h + 1e-6)
+    && (isNaN(s) ? isNaN(actual.s) && actual.s !== actual.s : s - 1e-6 <= actual.s && actual.s <= s + 1e-6)
+    && (isNaN(l) ? isNaN(actual.l) && actual.l !== actual.l : l - 1e-6 <= actual.l && actual.l <= l + 1e-6)
+    && (isNaN(opacity) ? isNaN(actual.opacity) && actual.opacity !== actual.opacity : actual.opacity === opacity)).toBe(true);
+
+  expect([actual.h, actual.s, actual.l, actual.opacity]).toEqual([h, s, l, opacity]);
 }
