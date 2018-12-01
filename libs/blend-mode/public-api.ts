5d5885b99af824acc00de28f41ae7a345ba36e9e
LinBo Len <linbolen@gradii.com>
2018-12-01 20:25:12 +0800

    blend

---
+++ libs/blend-mode/public-api.ts
@@ -0,0 +1,19 @@
+/**
+ * @licence
+ * Copyright (c) 2018 LinBo Len <linbolen@gradii.com>
+ * Copyright (c) 2010-2016 Mike Bostock (https://github.com/d3/d3-color)
+ *
+ * Use of this source code is governed by an MIT-style license.
+ * See LICENSE file in the project root for full license information.
+ */
+
+export * from './src/color-burn.blend';
+export * from './src/darken.blend';
+export * from './src/darker-color.blend';
+export * from './src/hue.blend';
+export * from './src/lighten.blend';
+export * from './src/lighter-color.blend';
+export * from './src/luminocity.blend';
+export * from './src/multiply.blend';
+export * from './src/normal.blend';
+export * from './src/saturation.blend';
